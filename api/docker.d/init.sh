3f553978b27c0f8833178f7b8f5f74cb18228c7b
Ben Hearsum <bhearsum@mozilla.com>
2019-09-18 10:56:54 -0400

    Enable SSL for backend.

--- api/docker.d/init.sh
+++ api/docker.d/init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exec /usr/local/bin/gunicorn shipit_api.flask:app --bind $HOST:$PORT --workers 2 --timeout 3600 --reload --reload-engine=poll --log-file -
+pushd `dirname $0` &>/dev/null
+MY_DIR=$(pwd)
+popd &>/dev/null
+
+cert="${MY_DIR}/cert.pem"
+key="${MY_DIR}/key.pem"
+
+exec /usr/local/bin/gunicorn shipit_api.flask:app --bind $HOST:$PORT --workers 2 --timeout 3600 --reload --reload-engine=poll --certfile=$cert --keyfile=$key --log-file -
