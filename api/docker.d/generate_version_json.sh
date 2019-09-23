364fb234d8960dbc8ab14a4241af05c37968c3c8
Ben Hearsum <bhearsum@mozilla.com>
2019-09-23 14:41:04 -0400

    Fix source link.

--- api/docker.d/generate_version_json.sh
+++ api/docker.d/generate_version_json.sh
@@ -1,7 +1,6 @@
 #/bin/sh
 set -e
 
-test $PROJECT_NAME
 test $GIT_HEAD_REV
 test $TASK_ID
 
@@ -9,7 +8,7 @@ cat > version.json <<EOF
 {
     "commit": "${GIT_HEAD_REV}",
     "version": "$(cat ./version.txt)",
-    "source": "https://github.com/mozilla-releng/${PROJECT_NAME}script",
+    "source": "https://github.com/mozilla-releng/shipit",
     "build": "https://tools.taskcluster.net/tasks/${TASK_ID}"
 }
 EOF
