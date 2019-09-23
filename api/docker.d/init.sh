a66d7f3f654f09808826e99271b218d0b2a22170
Ben Hearsum <bhearsum@mozilla.com>
2019-09-23 13:58:23 -0400

    Only apply most of the gunicorn config for local development.

--- api/docker.d/init.sh
+++ api/docker.d/init.sh
@@ -7,4 +7,11 @@ popd &>/dev/null
 cert="${MY_DIR}/cert.pem"
 key="${MY_DIR}/key.pem"
 
-exec /usr/local/bin/gunicorn shipit_api.flask:app --bind $HOST:$PORT --workers 2 --timeout 3600 --reload --reload-engine=poll --certfile=$cert --keyfile=$key --log-file -
+EXTRA_ARGS=""
+if [ $APP_CHANNEL == "development" ]
+then
+    # Local development only - we don't want these in deployed environments
+    EXTRA_ARGS="--bind $HOST:$PORT --workers 3 --timeout 3600 --reload --reload-engine=poll --certfile=$cert --keyfile=$key"
+fi
+
+exec /usr/local/bin/gunicorn shipit_api.flask:app --log-file - $EXTRA_ARGS
