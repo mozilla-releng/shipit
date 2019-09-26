9b2505d0018480c911008f664fc0163fd788b6ab
Ben Hearsum <bhearsum@mozilla.com>
2019-09-26 11:40:37 -0400

    Fix bash error.

--- api/docker.d/init.sh
+++ api/docker.d/init.sh
@@ -8,8 +8,10 @@ cert="${MY_DIR}/cert.pem"
 key="${MY_DIR}/key.pem"
 
 EXTRA_ARGS=""
-if [ $APP_CHANNEL == "development" ]
+if [ "$APP_CHANNEL" == "development" ]
 then
+    test $HOST
+    test $PORT
     # Local development only - we don't want these in deployed environments
     EXTRA_ARGS="--bind $HOST:$PORT --workers 3 --timeout 3600 --reload --reload-engine=poll --certfile=$cert --keyfile=$key"
 fi
