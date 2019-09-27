1748cd1709f057b725841e4e8237110ed4ddfaf6
Ben Hearsum <bhearsum@mozilla.com>
2019-09-27 10:32:45 -0400

    Add debug statements for cors config.

--- api/src/backend_common/cors.py
+++ api/src/backend_common/cors.py
@@ -14,6 +14,8 @@ cors = flask_cors.CORS()
 def init_app(app):
     origins = app.config.get("CORS_ORIGINS", "*").split(" ")
     resources = app.config.get("CORS_RESOURCES")
+    logger.debug(f"Set CORS_ORIGINS to {origins}")
+    logger.debug(f"Set CORS_RESOURCES to {resources}")
     if resources is not None:
         cors.init_app(app, resources=resources)
     else:
