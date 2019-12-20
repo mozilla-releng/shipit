0171a73f99aea15e6cc907ea78f322f963d35d77
Rail Aliiev <rail@mozilla.com>
2019-12-19 21:25:33 -0500

    GRANT won't apply on tables created after

--- api/docker.d/postgres_create_readonly_user.sh
+++ api/docker.d/postgres_create_readonly_user.sh
@@ -3,5 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER shipituserreadonly PASSWORD 'shipitpasswordreadonly';
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO shipituserreadonly;
+    ALTER DEFAULT PRIVILEGES FOR USER $POSTGRES_USER IN SCHEMA public GRANT SELECT ON TABLES TO shipituserreadonly;
 EOSQL
