90cdbe00c728bd622953c19effbe46ac1ec24f70
Rail Aliiev <rail@mozilla.com>
2020-02-13 09:20:17 -0500

    Switch to python 3.8 (#162)

--- api/src/cli_common/openapi_subset.py
+++ api/src/cli_common/openapi_subset.py
@@ -4,7 +4,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import sys
 
-import dpath
+import dpath.util
 import oyaml
 
 # Sections to be copied from the original document, separated by "." (period).
