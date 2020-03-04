d822040dc83aa44ef6f790b7c77cf9bdd9639243
Rail Aliiev <rail@mozilla.com>
2020-03-04 18:57:45 -0500

    Bug 1619704 - Move settings.py under src directory (#178)

--- api/src/shipit_api/admin/flask.py
+++ api/src/shipit_api/admin/flask.py
@@ -3,6 +3,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from pathlib import Path
+
 from shipit_api.admin import create_app
 
-app = create_app()
+settings = Path(".") / "settings.py"
+app = create_app(config=settings)
