424c7fabbfccfe172943a6207eeb69e424b592f9
Tom Prince <mozilla@hocat.ca>
2020-05-04 14:34:32 -0600

    Remove some obselete ways of setting configuration. (#212)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,17 +1,7 @@
-const getConfigFromBody = (name, _default) => {
-  let url = document.body.getAttribute(`data-${name}`);
-  if (url === null) {
-    url = _default;
-  }
-  if (url === undefined) {
-    throw Error(`You need to set "data-${name}"`);
-  }
-  return url;
-};
-
-export const SHIPIT_API_URL = getConfigFromBody('shipit-api-url', process.env.SHIPIT_API_URL);
-export const SHIPIT_PUBLIC_API_URL = getConfigFromBody('shipit-public-api-url', process.env.SHIPIT_PUBLIC_API_URL);
-export const TASKCLUSTER_ROOT_URL = getConfigFromBody('frontend-taskcluster-root-url', process.env.FRONTEND_TASKCLUSTER_ROOT_URL);
-export const RELEASE_CHANNEL = getConfigFromBody('release-channel', process.env.RELEASE_CHANNEL);
-export const SENTRY_DSN = getConfigFromBody('sentry-dsn', process.env.SENTRY_DSN || null);
+/* eslint-disable prefer-destructuring */
+export const SHIPIT_API_URL = process.env.SHIPIT_API_URL;
+export const SHIPIT_PUBLIC_API_URL = process.env.SHIPIT_PUBLIC_API_URL;
+export const TASKCLUSTER_ROOT_URL = process.env.FRONTEND_TASKCLUSTER_ROOT_URL;
+export const RELEASE_CHANNEL = process.env.RELEASE_CHANNEL;
+export const SENTRY_DSN = process.env.SENTRY_DSN || null;
 export default require(`./configs/${RELEASE_CHANNEL}`); // eslint-disable-line import/no-dynamic-require, global-require
