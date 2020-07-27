8329e34258a6e9249e24fdcbb420399d4942b886
Tom Prince <mozilla@hocat.ca>
2020-07-27 12:38:20 -0600

    Bump nightly version and release dates.

--- api/src/shipit_api/common/config.py
+++ api/src/shipit_api/common/config.py
@@ -46,17 +46,17 @@ ESR_BRANCH_PREFIX = "releases/mozilla-esr"
 # day).
 # We could have used the in-tree version, but there can be race conditions,
 # e.g. version bumped, but still no builds available.
-FIREFOX_NIGHTLY = "80.0a1"
+FIREFOX_NIGHTLY = "81.0a1"
 
 # The next 6 dates are information about the current and next release
 # They must be updated at the same time as FIREFOX_NIGHTLY
 # They can be found: https://wiki.mozilla.org/Release_Management/Calendar
-LAST_SOFTFREEZE_DATE = "2020-04-30"
-LAST_MERGE_DATE = "2020-05-04"
-LAST_RELEASE_DATE = "2020-05-05"
-NEXT_SOFTFREEZE_DATE = "2020-05-28"
-NEXT_MERGE_DATE = "2020-06-01"
-NEXT_RELEASE_DATE = "2020-06-02"
+LAST_SOFTFREEZE_DATE = "2020-07-23"
+LAST_MERGE_DATE = "2020-07-27"
+LAST_RELEASE_DATE = "2020-07-28"
+NEXT_SOFTFREEZE_DATE = "2020-08-20"
+NEXT_MERGE_DATE = "2020-08-24"
+NEXT_RELEASE_DATE = "2020-08-25"
 
 # Fennec Nightly users have been migrated to Fenix, thus we don't need to bump
 # the following numbers anymore.
