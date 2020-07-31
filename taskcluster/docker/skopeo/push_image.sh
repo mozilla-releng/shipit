e40fe310d3234bd3e0580217df42685ff368a895
Rail Aliiev <rail@mozilla.com>
2020-07-30 23:05:03 -0400

    Inject version.json on push
    In order to the cloudops deployment automation work, version.json should
    point to the task in which the docker image digest is printed. This
    happens only after the image is pushed. This means that we need to
    generate the version.json file as a part of the push task.

--- taskcluster/docker/skopeo/push_image.sh
+++ taskcluster/docker/skopeo/push_image.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
 set -e
 
-test $DOCKER_REPO
+test $APP_VERSION
 test $DEPLOYMENT_BRANCH
 test $DEPLOY_SECRET_URL
+test $DOCKER_REPO
 test $MOZ_FETCHES_DIR
+test $TASKCLUSTER_ROOT_URL
+test $TASK_ID
+test $VCS_HEAD_REPOSITORY
+test $VCS_HEAD_REV
 
 echo "=== Generating dockercfg ==="
 mkdir -m 700 $HOME/.docker
 curl $DEPLOY_SECRET_URL | jq '.secret.docker.skopeo' > $HOME/.docker/config.json
 chmod 600 $HOME/.docker/config.json
 
+cd $MOZ_FETCHES_DIR
+unzstd image.tar.zst
+
+echo "=== Inserting version.json into image ==="
+# Create an OCI copy of image in order umoci can patch it
+skopeo copy docker-archive:image.tar oci:shipit:final
+
+cat > version.json <<EOF
+{
+    "commit": "${VCS_HEAD_REV}",
+    "version": "${APP_VERSION}",
+    "source": "${VCS_HEAD_REPOSITORY}",
+    "build": "${TASKCLUSTER_ROOT_URL}/tasks/${TASK_ID}"
+}
+EOF
+
+umoci insert --image shipit:final version.json /app/version.json
+
 echo "=== Pushing to docker hub ==="
 DOCKER_ARCHIVE_TAG="${DEPLOYMENT_BRANCH}-$(date +%Y%m%d%H%M%S)"
-unzstd $MOZ_FETCHES_DIR/image.tar.zst
-skopeo copy docker-archive:$MOZ_FETCHES_DIR/image.tar docker://$DOCKER_REPO:$DEPLOYMENT_BRANCH
+skopeo copy oci:shipit:final docker://$DOCKER_REPO:$DEPLOYMENT_BRANCH
 # Print the digest to make the cloudops tooling work
 skopeo inspect docker://$DOCKER_REPO:$DEPLOYMENT_BRANCH
-skopeo copy docker-archive:$MOZ_FETCHES_DIR/image.tar docker://$DOCKER_REPO:$DOCKER_ARCHIVE_TAG
+skopeo copy oci:shipit:final docker://$DOCKER_REPO:$DOCKER_ARCHIVE_TAG
 skopeo inspect docker://$DOCKER_REPO:$DOCKER_ARCHIVE_TAG
 
 echo "=== Clean up ==="
--- taskcluster/shipit_taskgraph/transforms/docker_push.py
+++ taskcluster/shipit_taskgraph/transforms/docker_push.py
@@ -10,6 +10,9 @@ Kubernetes docker image builds.
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import os
+import six
+
 from taskgraph.transforms.base import TransformSequence
 
 transforms = TransformSequence()
@@ -23,7 +26,18 @@ def set_push_environment(config, jobs):
         if not config.params.get("deploy"):
             continue
 
-        env = job["worker"].setdefault("env", {})
-        env.update({"DEPLOYMENT_BRANCH": config.params["deployment_branch"]})
-        yield job
+        version_file = os.path.join(config.graph_config.vcs_root, "api",
+                                    "version.txt")
+        version = open(version_file).read().strip()
+        version_txt = six.ensure_text(version)
 
+        env = job["worker"].setdefault("env", {})
+        env.update({
+            "DEPLOYMENT_BRANCH": config.params["deployment_branch"],
+            "VCS_HEAD_REPOSITORY": config.params['head_repository'],
+            "VCS_HEAD_REV": config.params['head_rev'],
+            # TODO: Figure out if we still need to add the version
+            "APP_VERSION": version_txt,
+
+        })
+        yield job
