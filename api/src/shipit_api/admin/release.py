dc4af747a426c4f08c13fbde15414da6fdf69abd
Ben Hearsum <bhearsum@mozilla.com>
2020-09-11 16:13:42 -0400

    Turn partner builds on earlier in the beta cycle (#443)

--- api/src/shipit_api/admin/release.py
+++ api/src/shipit_api/admin/release.py
@@ -94,7 +94,7 @@ def is_partner_enabled(product, version, min_version=60):
     major_version = int(version.split(".")[0])
     if product == "firefox" and major_version >= min_version:
         if is_beta(version):
-            if get_beta_num(version) >= 8:
+            if get_beta_num(version) >= 5:
                 return True
         elif is_esr(version):
             return True
