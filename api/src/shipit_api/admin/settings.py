f4532dc32e26fc6bb6bd99d55f046e89baf407d0
Ben Hearsum <bhearsum@mozilla.com>
2020-09-16 13:07:42 -0400

    Add mark and dale to privileged xpi build group

--- api/src/shipit_api/admin/settings.py
+++ api/src/shipit_api/admin/settings.py
@@ -67,7 +67,13 @@ ADMIN_GROUP = [
 ]
 
 # Please ping awagner before making changes to this group
-XPI_PRIVILEGED_BUILD_GROUP = ["amarchesini@mozilla.com", "rdalal@mozilla.com", "mcooper@mozilla.com"]
+XPI_PRIVILEGED_BUILD_GROUP = [
+    "amarchesini@mozilla.com",
+    "rdalal@mozilla.com",
+    "mcooper@mozilla.com",
+    "mbanner@mozilla.com",
+    "dharvey@mozilla.com",
+]
 XPI_PRIVILEGED_ADMIN_GROUP = [
     "awagner@mozilla.com",
     "pkewisch@mozilla.com",
