475a18f87aa77e753da4535b0024c16a6e26b28e
Sarah Clements <sclements313@gmail.com>
2021-06-07 14:01:31 -0700

    Issue #426 - show metadata about releases (#761)
    * Add revision and date to cards
    * Add signoff completed_by to cards for XPI releases
    * Add MouseOverPopover to Shared directory

---
+++ frontend/src/components/Shared/MouseOverPopover.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Popover from '@material-ui/core/Popover';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function MouseOverPopover({
+  popoverContent,
+  text,
+  fontSize,
+  fontWeight,
+  position,
+}) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handlePopoverOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <div>
+      <Box
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+        fontSize={fontSize}
+        fontWeight={fontWeight}
+        position={position}>
+        {text}
+      </Box>
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus>
+        <Box>{popoverContent}</Box>
+      </Popover>
+    </div>
+  );
+}
+
+MouseOverPopover.defaultProps = {
+  position: 'relative',
+};
