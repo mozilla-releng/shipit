37e8b0f4deb6a4b71307f3b468c98772fd6ba54a
Gabriel Bustamante <gabriel@mozilla.com>
2021-12-20 09:27:17 -0600

    black

--- api/src/shipit_api/admin/tasks.py
+++ api/src/shipit_api/admin/tasks.py
@@ -3,9 +3,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
 import copy
 import json
+import os
 from functools import lru_cache
 
 import jsone
