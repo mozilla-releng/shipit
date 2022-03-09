b91878faa6315c19b7378849b0f1788e6da86bb6
Andrew Halberstadt <ahal@mozilla.com>
2022-03-09 09:31:55 -0500

    decision: bootstrap Taskgraph from pypi (#1020)
    Jira: RELENG-817

--- taskcluster/shipit_taskgraph/transforms/version_json.py
+++ taskcluster/shipit_taskgraph/transforms/version_json.py
@@ -6,13 +6,9 @@
 Generate version.json args
 """
 
-import json
-import six
 import os
-from copy import deepcopy
 
 from taskgraph.transforms.base import TransformSequence
-from taskgraph.util.taskcluster import get_root_url
 
 transforms = TransformSequence()
 
@@ -24,12 +20,11 @@ def add_version_json_args(config, jobs):
         version_file = os.path.join(config.graph_config.vcs_root, "api",
                                     "version.txt")
         version = open(version_file).read().strip()
-        version_txt = six.ensure_text(version)
         args = job.get('args', {})
 
         args["VCS_HEAD_REPOSITORY"] = config.params['head_repository']
         # TODO: Figure out if we still need to add the version
-        args["APP_VERSION"] = version_txt
+        args["APP_VERSION"] = version
         # TODO: VCS_HEAD_REV should not affect the docker digest and cause
         # rebuilds on every push
         args["VCS_HEAD_REV"] = config.params['head_rev']
