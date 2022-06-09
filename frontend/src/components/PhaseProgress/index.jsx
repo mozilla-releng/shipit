032adf1f60e61c8a02a7ae151640325aa2ff8080
Gabriel Bustamante <gbustamante@mozilla.com>
2022-06-09 17:42:50 -0500

    block-xpi-releases-only-on-failed-task-group-tasks

--- frontend/src/components/PhaseProgress/index.jsx
+++ frontend/src/components/PhaseProgress/index.jsx
@@ -177,7 +177,8 @@ export default function PhaseProgress({ release, readOnly, xpi }) {
       (idx === 0 || // The first phase can be scheduled anytime
       allowPhaseSkipping || // Can schedule anything
       phases[idx - 1].tcStatus === 'completed' || // previsous phase is done
-        phases[idx - 1].tcStatus === 'warning' ||
+        // Block XPI releases if there are failed tasks in the Phase's task group
+        (phases[idx - 1].tcStatus === 'warning' && !('xpi_name' in release)) ||
         // Special case for Firefox RC.
         // push_firefox can be scheduled even if ship_firefox_rc (the previous
         // phase) is not ready. We still need to be sure that
