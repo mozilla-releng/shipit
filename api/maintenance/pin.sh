9ced70017af0b304d639539da580a35b80ea4a34
Ben Hearsum <ben@mozilla.com>
2023-03-23 14:28:17 -0400

    Replace setup.py and requirements files with pyproject.toml and poetry

--- api/maintenance/pin.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# We run this in shipit/api/. to pin all requirements.
-#
-# Usage: maintenance/pin.sh [<extra-pip-compile-multi-arguments>]
-#
-set -e
-set -x
-
-docker run -t -v $PWD:/src -w /src python:3.9 maintenance/pin-helper.sh $@
