316a47491e244bced377995748b1da5bd4896e25
Gabriel Bustamante <gbustamante@mozilla.com>
2023-07-11 12:04:02 -0500

    refactor-tab-nav-and-add-vpn

---
+++ frontend/src/components/Dashboard/ReleasesMenu.js
@@ -0,0 +1,126 @@
+import React, { useState, Fragment } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+import Menu from "@material-ui/core/Menu";
+import menuItems from './menuItems';
+import { withUser } from "../../utils/AuthContext";
+import SettingsOutlineIcon from "mdi-react/RocketIcon";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+export function hasChildren(item) {
+  const { items: children } = item;
+
+  if (children === undefined) {
+    return false;
+  }
+
+  if (children.constructor !== Array) {
+    return false;
+  }
+
+  if (children.length === 0) {
+    return false;
+  }
+
+  return true;
+}
+
+const MenuItem = ({ item }) => {
+  const Component = hasChildren(item) ? MultiLevel : SingleLevel;
+  return <Component item={item} />;
+};
+
+const SingleLevel = ({ item }) => {
+  const classes = useStyles();
+  return (
+    <ListItem button className={classes.singleLevel}>
+      <ListItemText primary={item.title} />
+    </ListItem>
+  );
+};
+
+const MultiLevel = ({ item }) => {
+  const { items: children } = item;
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <React.Fragment>
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary={item.title} />
+        {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {children.map((child, key) => (
+            <MenuItem key={key} item={child} />
+          ))}
+        </List>
+      </Collapse>
+    </React.Fragment>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    height: theme.spacing(6),
+    width: theme.spacing(6),
+    margin: `0 ${theme.spacing(1)}px`,
+  },
+  singleLevel: {
+    paddingLeft: '40px',
+  },
+  menu: {
+    // width: '100%',
+    // padding: '10%'
+  }
+}));
+
+function MenuItems() {
+  return menuItems.map((item, key) => <MenuItem key={key} item={item} />);
+}
+
+function SettingsMenu({ user, disabled }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuOpen = (e) => setAnchorEl(e.currentTarget);
+  const handleMenuClose = () => setAnchorEl(null);
+
+  return (
+    <Fragment>
+      <Button
+        disabled={!user || disabled}
+        className={classes.button}
+        aria-haspopup="true"
+        aria-controls="user-menu"
+        aria-label="user menu"
+        startIcon={<SettingsOutlineIcon />}
+        onClick={handleMenuOpen}
+      >
+        Releases
+      </Button>
+      <Menu
+        id="user-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        onClose={handleMenuClose}
+        className={classes.menu}
+      >
+        <MenuItems />
+      </Menu>
+    </Fragment>
+  );
+}
+
+export default withUser(SettingsMenu);
--- frontend/src/components/Dashboard/menuItems.js
+++ frontend/src/components/Dashboard/menuItems.js
@@ -1,28 +1,53 @@
-export default {
-  main: [
-    {
-      value: 'Releases',
-      path: '/',
-    },
-    {
-      value: 'New Release',
-      path: '/new',
-    },
-    {
-      value: 'Recent Releases',
-      path: '/recent',
-    },
-    {
-      value: 'XPI Releases',
-      path: '/xpi',
-    },
-    {
-      value: 'New XPI Release',
-      path: '/newxpi',
-    },
-    {
-      value: 'Recent XPI Releases',
-      path: '/recentxpi',
-    },
-  ],
-};
+export default [
+  {
+    title: "Firefox",
+    items: [
+      {
+        title: "Pending Releases",
+        to: "/",
+      },
+      {
+        title: "New Firefox Release",
+        to: "/new",
+      },
+      {
+        title: "Recent Firefox Releases",
+        to: "/recent",
+      },
+    ],
+  },
+  {
+    title: "Mozilla Extensions",
+    items: [
+      {
+        title: "Pending XPI Releases",
+        to: "/xpi",
+      },
+      {
+        title: "New XPI Release",
+        to: "/newxpi",
+      },
+      {
+        title: "Recent XPI Releases",
+        to: "/recentxpi",
+      },
+    ],
+  },
+  {
+    title: "Mozilla VPN",
+    items: [
+      {
+        title: "Pending Mozilla Releases",
+        to: "/mozillavpn",
+      },
+      {
+        title: "New Mozilla VPN Release",
+        to: "/newmozillavpn",
+      },
+      {
+        title: "Recent Mozilla VPN Releases",
+        to: "/recentmozillavpn",
+      },
+    ],
+  },
+];
