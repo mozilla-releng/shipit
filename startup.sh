15c547ff04e38be11561d35ba10f039afe987034
Andrew Halberstadt <ahal@mozilla.com>
2023-07-12 10:17:08 -0400

    Bug 1842897 - Make some improvements to startup script

--- startup.sh
+++ startup.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
+export TASKCLUSTER_ROOT_URL="${TASKCLUSTER_ROOT_URL:-https://firefox-ci-tc.services.mozilla.com}"
 
-set -e
+scopes=('hooks:trigger-hook:project-comm/in-tree-action-1-generic/*' \
+        'hooks:trigger-hook:project-comm/in-tree-action-1-release-promotion/*' \
+        'hooks:trigger-hook:project-gecko/in-tree-action-1-generic/*' \
+        'hooks:trigger-hook:project-gecko/in-tree-action-1-release-promotion/*' \
+        'hooks:trigger-hook:project-mobile/in-tree-action-1-generic/*' \
+        'hooks:trigger-hook:project-mobile/in-tree-action-1-release-promotion/*' \
+        'hooks:trigger-hook:project-xpi/in-tree-action-1-release-promotion/*')
 
-scopes=('-s' 'hooks:trigger-hook:project-comm/in-tree-action-1-generic/*' \
-        '-s' 'hooks:trigger-hook:project-comm/in-tree-action-1-release-promotion/*' \
-        '-s' 'hooks:trigger-hook:project-gecko/in-tree-action-1-generic/*' \
-        '-s' 'hooks:trigger-hook:project-gecko/in-tree-action-1-release-promotion/*' \
-        '-s' 'hooks:trigger-hook:project-mobile/in-tree-action-1-generic/*' \
-        '-s' 'hooks:trigger-hook:project-mobile/in-tree-action-1-release-promotion/*' \
-        '-s' 'hooks:trigger-hook:project-xpi/in-tree-action-1-release-promotion/*')
-export TASKCLUSTER_ROOT_URL='https://firefox-ci-tc.services.mozilla.com/' && eval $(taskcluster signin ${scopes[@]})
-echo -n "Please enter your github token: "
-read _token
-export GITHUB_TOKEN="$_token"
-docker-compose up
+function signin {
+    scope_str=$(IFS=$'\n' ; echo "${scopes[*]}")
+    eval "$(taskcluster signin --scope="$scope_str")"
+}
+
+if [[ -z "${TASKCLUSTER_CLIENT_ID}" ]]; then
+    signin
+else
+    active_scopes=$(taskcluster api auth currentScopes)
+    for scope in "${scopes[@]}"
+    do
+        if [[ $active_scopes != *"$scope"* ]]; then
+            signin
+            break
+        fi
+    done
+fi
+
+if [[ -z "${GITHUB_TOKEN}" ]]; then
+    echo -n "Please enter your github token: "
+    read -r _token
+    export GITHUB_TOKEN="$_token"
+fi
+docker compose up
+>>>>>>> 95a90b7... Bug 1842897 - Make some improvements to startup script
