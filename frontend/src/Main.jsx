908a78212e2b8c1090792b0187d240ce803d21a4
Gabriel Bustamante <gbustamante@mozilla.com>
2023-08-02 11:33:41 -0500

    Update dashboard titles to match breadcrumbs

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -33,7 +33,7 @@ function Main() {
   if (backendStatus.loading) {
     return (
       <BrowserRouter>
-        <Dashboard group="Loading..." disabled>
+        <Dashboard disabled>
           <Spinner loading />
         </Dashboard>
       </BrowserRouter>
@@ -43,7 +43,7 @@ function Main() {
   if (backendStatus.error) {
     return (
       <BrowserRouter>
-        <Dashboard title="Error" disabled>
+        <Dashboard disabled>
           <ErrorPanel
             fixed
             error={`Error contacting Shipit backend: ${backendStatus.error.toString()}. Are you connected to the VPN?`}
