5d65fd025ddf062d985eb012dccd341f00a8e0db
Johan Lorenzo <jlorenzo@mozilla.com>
2024-02-13 12:08:34 +0100

    test: Add test for _check_mandatory_keys_are_provided() (bug 1879094)

--- api/tests/test_backend_common.py
+++ api/tests/test_backend_common.py
@@ -1,3 +1,7 @@
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
 import backend_common
 
 
@@ -30,3 +34,23 @@ def test_set_products_config_default_values():
             "repo-url": "https://github.com/some-org/some-repo",
         },
     }
+
+
+@pytest.mark.parametrize(
+    "products_config, expectation",
+    (
+        (
+            {"mandatory-keys-provided": {"version-class": "some.version.Class"}},
+            does_not_raise(),
+        ),
+        (
+            {
+                "missing-version-class": {},
+            },
+            pytest.raises(KeyError),
+        ),
+    ),
+)
+def test_check_mandatory_keys_are_provided(products_config, expectation):
+    with expectation:
+        backend_common._check_mandatory_keys_are_provided(products_config)
