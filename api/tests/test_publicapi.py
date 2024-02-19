mozilla-releng/shipit

commit ff21396f3350d7db467b69ea7e4c367b1a0281b4

refactor: Avoid redefining version parsing twice in backend (bug 1879094)

api/src/shipit_api/public/api.py
@@ -7,30 +7,12 @@ import logging
 from collections import defaultdict
 
 from flask import abort, current_app
-from mozilla_version.fenix import FenixVersion
-from mozilla_version.gecko import DeveditionVersion, FennecVersion, FirefoxVersion, GeckoVersion, ThunderbirdVersion
-from mozilla_version.mobile import MobileVersion
-from mozilla_version.version import BaseVersion
 from werkzeug.exceptions import BadRequest
 
 from shipit_api.common.models import DisabledProduct, Phase, Release, XPIRelease
-from shipit_api.common.product import Product
+from shipit_api.common.version import parse_version
 
 logger = logging.getLogger(__name__)
-
-VERSION_CLASSES = {
-    Product.ANDROID_COMPONENTS.value: MobileVersion,
-    Product.APP_SERVICES.value: GeckoVersion,
-    Product.DEVEDITION.value: DeveditionVersion,
-    Product.FENIX.value: FenixVersion,
-    Product.FENNEC.value: FennecVersion,
-    Product.FIREFOX.value: FirefoxVersion,
-    Product.FIREFOX_ANDROID.value: MobileVersion,
-    Product.FOCUS_ANDROID.value: MobileVersion,
-    Product.MOZILLA_VPN_ADDONS.value: BaseVersion,
-    Product.MOZILLA_VPN_CLIENT.value: BaseVersion,
-    Product.THUNDERBIRD.value: ThunderbirdVersion,
-}
 
 
 def good_version(release):
@@ -41,11 +23,8 @@ def good_version(release):
     Example versions that cannot be parsed:
     1.1, 1.1b1, 2.0.0.1
     """
-    product = release["product"]
-    if product not in VERSION_CLASSES:
-        raise ValueError(f"Product {product} versions are not supported")
     try:
-        VERSION_CLASSES[product].parse(release["version"])
+        parse_version(release["version"])
         return True
     except ValueError:
         return False
@@ -80,7 +59,7 @@ def _sort_releases_by_product_then_version(releases):
         releases_for_product.append(release)
 
     for product, releases in releases_by_product.items():
-        releases_by_product[product] = sorted(releases, key=lambda r: VERSION_CLASSES[product].parse(r["version"]))
+        releases_by_product[product] = sorted(releases, key=lambda r: parse_version(r["version"]))
 
     return [release for product in sorted(releases_by_product.keys()) for release in releases_by_product[product]]
 

api/src/shipit_api/admin/release.py
@@ -3,49 +3,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import importlib
 import logging
-from functools import cache
 
 import requests
 from mozilla_version.gecko import FirefoxVersion
 
-from backend_common import get_products_config
 from shipit_api.common.config import SUPPORTED_FLAVORS
-from shipit_api.common.product import Product, get_key
+from shipit_api.common.product import Product
+from shipit_api.common.version import parse_version
 
 logger = logging.getLogger(__name__)
-
-
-@cache
-def _get_version_class_per_product_name():
-    version_class_per_product = {}
-    for product_name, product_config in get_products_config().items():
-        product_enum = Product[get_key(product_name)]
-        version_class_path = product_config["version-class"]
-        module_path = ".".join(version_class_path.split(".")[:-1])
-        class_name = version_class_path.split(".")[-1]
-        VersionClass = getattr(importlib.import_module(module_path), class_name, None)
-        version_class_per_product[product_enum] = VersionClass
-
-    return version_class_per_product
-
-
-def parse_version(product, version):
-    if isinstance(product, Product):
-        product_enum = product
-    else:
-        try:
-            product_enum = Product[get_key(product)]
-        except KeyError:
-            raise ValueError(f"Product {product} versions are not supported")
-
-    try:
-        VersionClass = _get_version_class_per_product_name()[product_enum]
-    except KeyError:
-        raise ValueError(f"Product {product} versions are not supported")
-
-    return VersionClass.parse(version)
 
 
 def is_rc(product, version, partial_updates):

api/src/shipit_api/common/version.py
@@ -1,0 +1,36 @@
+import importlib
+from functools import cache
+
+from backend_common import get_products_config
+from shipit_api.common.product import Product, get_key
+
+
+@cache
+def _get_version_class_per_product_name():
+    version_class_per_product = {}
+    for product_name, product_config in get_products_config().items():
+        product_enum = Product[get_key(product_name)]
+        version_class_path = product_config["version-class"]
+        module_path = ".".join(version_class_path.split(".")[:-1])
+        class_name = version_class_path.split(".")[-1]
+        VersionClass = getattr(importlib.import_module(module_path), class_name, None)
+        version_class_per_product[product_enum] = VersionClass
+
+    return version_class_per_product
+
+
+def parse_version(product, version):
+    if isinstance(product, Product):
+        product_enum = product
+    else:
+        try:
+            product_enum = Product[get_key(product)]
+        except KeyError:
+            raise ValueError(f"Product {product} versions are not supported")
+
+    try:
+        VersionClass = _get_version_class_per_product_name()[product_enum]
+    except KeyError:
+        raise ValueError(f"Product {product} versions are not supported")
+
+    return VersionClass.parse(version)

commit 2b1ff904b6c365e7cc2b9c93c99926f6426a0598

fix: Wrong call to parse_version() in _sort_releases_by_product_then_version() (bug 1879094)
Fixes regression introduced in ff21396f3350d7db467b69ea7e4c367b1a0281b4

api/src/shipit_api/public/api.py
@@ -59,7 +59,7 @@ def _sort_releases_by_product_then_version(releases):
         releases_for_product.append(release)
 
     for product, releases in releases_by_product.items():
-        releases_by_product[product] = sorted(releases, key=lambda r: parse_version(r["version"]))
+        releases_by_product[product] = sorted(releases, key=lambda r: parse_version(r["product"], r["version"]))
 
     return [release for product in sorted(releases_by_product.keys()) for release in releases_by_product[product]]
 

api/tests/test_publicapi.py
@@ -31,3 +31,28 @@ def test_public_api_subset():
 )
 def test_good_version(release, expected):
     assert api.good_version(release) == expected
+
+
+@pytest.mark.parametrize(
+    "releases, expected",
+    (
+        (
+            [
+                {"product": "firefox", "version": "123.0"},
+                {"product": "firefox", "version": "123.0b1"},
+                {"product": "firefox", "version": "122.0.1"},
+            ],
+            [{"product": "firefox", "version": "122.0.1"}, {"product": "firefox", "version": "123.0b1"}, {"product": "firefox", "version": "123.0"}],
+        ),
+        (
+            [
+                {"product": "firefox", "version": "123.0"},
+                {"product": "thunderbird", "version": "123.0"},
+                {"product": "firefox-android", "version": "123.0"},
+            ],
+            [{"product": "firefox", "version": "123.0"}, {"product": "firefox-android", "version": "123.0"}, {"product": "thunderbird", "version": "123.0"}],
+        ),
+    ),
+)
+def test_sort_releases_by_product_then_version(releases, expected):
+    assert api._sort_releases_by_product_then_version(releases) == expected