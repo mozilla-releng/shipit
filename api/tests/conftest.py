b349ec43adede3f91dbcd88e86fb0d6bac730140
Bastien Orivel <borivel@mozilla.com>
2025-05-27 14:47:19 +0200

    Add a test for `add_release`

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -35,6 +35,7 @@ def app():
             "TASKCLUSTER_CLIENT_ID": "something",
             "TASKCLUSTER_ACCESS_TOKEN": "something",
             "AUTH0_AUTH_SCOPES": {
+                "project:releng:services/shipit_api/add_release/firefox": "releng",
                 "project:releng:services/shipit_api/schedule_phase/firefox/promote": "releng",
                 "project:releng:services/shipit_api/schedule_phase/firefox/ship": "releng",
             },
