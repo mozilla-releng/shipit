6381a4e3ff0d16ea364affc08a41d57cc4357691
Bastien Orivel <borivel@mozilla.com>
2025-07-23 14:35:28 +0200

    Change from react-time-ago to react-timeago (#1717)
    I was looking at updating the former and while searching I found the
    latter which is pretty much as used but is way leaner, since the
    usage we make of this library is very minimal, we might as well.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import JavascriptTimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
 import App from './App';
-
-JavascriptTimeAgo.addLocale(en);
 
 render(<App />, document.getElementById('root'));
