7697b2a3c99083865fa32a443d5e410af5819b26
Bastien Orivel <borivel@mozilla.com>
2025-09-01 11:28:22 +0200

    Refactor the dashboard menu to reduce repetition (#1775)
    I'm looking at adding a third menu, this makes it a lot less painful

---
+++ frontend/src/components/Dashboard/DashboardMenu.jsx
@@ -0,0 +1,76 @@
+import { withAuth0 } from '@auth0/auth0-react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import Typography from '@mui/material/Typography';
+import React, { Fragment, useState } from 'react';
+import { makeStyles } from 'tss-react/mui';
+
+const useStyles = makeStyles()(() => ({
+  button: {
+    color: '#fff',
+    display: 'flex',
+    textTransform: 'none',
+    marginRight: '.15%',
+  },
+  endIcon: {
+    margin: '0px',
+  },
+  icon: {
+    fill: '#fff',
+  },
+}));
+
+function DashboardMenu({
+  title,
+  icon,
+  children,
+  disabled,
+  ariaLabel,
+  menuId,
+  auth0,
+}) {
+  const { classes } = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuOpen = (e) => setAnchorEl(e.currentTarget);
+  const handleMenuClose = () => setAnchorEl(null);
+
+  if (!auth0.user || disabled) {
+    return '';
+  }
+
+  return (
+    <Fragment>
+      <Button
+        className={classes.button}
+        classes={{
+          endIcon: classes.endIcon,
+        }}
+        aria-haspopup="true"
+        aria-controls={menuId}
+        aria-label={ariaLabel || title.toLowerCase()}
+        startIcon={React.cloneElement(icon, { className: classes.icon })}
+        endIcon={<ArrowDropDownIcon className={classes.icon} />}
+        onClick={handleMenuOpen}
+      >
+        <Typography variant="h6" noWrap>
+          {title}
+        </Typography>
+      </Button>
+      <Menu
+        id={menuId}
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        onClose={handleMenuClose}
+        slotProps={{
+          root: { sx: { '.MuiList-root': { padding: 0 } } },
+        }}
+      >
+        {children}
+      </Menu>
+    </Fragment>
+  );
+}
+
+export default withAuth0(DashboardMenu);
