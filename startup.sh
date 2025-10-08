5a0bb2d15509e68125e2803a6e5d31393491f413
Bastien Orivel <borivel@mozilla.com>
2025-10-08 15:08:05 +0200

    Add scopes to the local dev environment to be able to cancel and seal task groups
    This is required to make use of the `cancel_action_task_group` function

--- startup.sh
+++ startup.sh
@@ -13,6 +13,11 @@ scopes=('hooks:trigger-hook:project-app-services/in-tree-action-1-generic/*' \
         'hooks:trigger-hook:project-mozillavpn/in-tree-action-1-release-promotion/*' \
         'hooks:trigger-hook:project-xpi/in-tree-action-1-release-promotion/*')
 
+for project in "app-services" "gecko" "mobile" "mozillavpn" "xpi"; do
+    # These scopes can be commented out if you don't have them on the target taskcluster instance.
+    scopes+=("queue:cancel-task:${project}-level-1/*" "queue:seal-task-group:${project}-level-1/*")
+done
+
 function signin {
     scope_str=$(IFS=$'\n' ; echo "${scopes[*]}")
     eval "$(taskcluster signin --scope="$scope_str")"
