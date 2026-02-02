a6901df94040ce4eb6eb56c714adacb34022e41c
Bastien Orivel <borivel@mozilla.com>
2026-02-02 11:51:04 +0100

    Add an API route to list products supported by merge automation

--- api/src/shipit_api/admin/merge_automation.py
+++ api/src/shipit_api/admin/merge_automation.py
@@ -22,6 +22,10 @@ from shipit_api.common.models import MergeAutomation, TaskStatus
 logger = logging.getLogger(__name__)
 
 
+def list_products():
+    return list(MERGE_BEHAVIORS_PER_PRODUCT.keys())
+
+
 def get_behavior_for_product(product, behavior_name):
     if product not in MERGE_BEHAVIORS_PER_PRODUCT:
         abort(404, f"No merge behavior found for product: {product}")
