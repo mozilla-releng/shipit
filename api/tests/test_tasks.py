b673c6910e47a61dca06943611075cd094bb4952
Bastien Orivel <borivel@mozilla.com>
2026-02-17 17:04:35 +0100

    Bug 2014837 - Remove promote RC and the concept of RC in new releases

--- api/tests/test_tasks.py
+++ api/tests/test_tasks.py
@@ -1,5 +1,4 @@
 from contextlib import nullcontext as does_not_raise
-from copy import deepcopy
 from unittest import mock
 
 import pytest
@@ -12,14 +11,14 @@ from shipit_api.admin import tasks
     "avail_flavors, product, version, partial_updates, product_key, expectation, result",
     (
         (
-            ["promote_firefox", "promote_firefox_rc", "push_firefox", "ship_firefox"],
+            ["promote_firefox", "push_firefox", "ship_firefox"],
             "firefox",
             "100.0",
             None,
             None,
             does_not_raise(),
             [
-                {"name": "promote_firefox_rc", "in_previous_graph_ids": True},
+                {"name": "promote_firefox", "in_previous_graph_ids": True},
                 {"name": "push_firefox", "in_previous_graph_ids": True},
                 {"name": "ship_firefox", "in_previous_graph_ids": True},
             ],
@@ -50,7 +49,7 @@ from shipit_api.admin import tasks
                 {"name": "ship_firefox", "in_previous_graph_ids": True},
             ],
         ),
-        (["some-flavor"], "non-existing-product", "1.0", None, None, pytest.raises(ValueError), None),
+        (["some-flavor"], "non-existing-product", "1.0", None, None, pytest.raises(tasks.UnsupportedFlavor), None),
         (
             ["promote_firefox", "push_firefox"],
             "firefox",
@@ -100,13 +99,10 @@ def test_extract_our_flavors_unsupported_flavor():
     # This error is only raised when someone adds a new product in
     # release._VERSION_CLASS_PER_PRODUCT but forgets to add that
     # new product in config.SUPPORTED_FLAVORS.
-    supported_flavors = deepcopy(tasks.SUPPORTED_FLAVORS)
-
-    del tasks.SUPPORTED_FLAVORS["firefox"]
-    with pytest.raises(tasks.UnsupportedFlavor):
-        tasks.extract_our_flavors([], "firefox", "100.0.1", None)
-
-    tasks.SUPPORTED_FLAVORS = supported_flavors
+    flavors_without_firefox = {k: v for k, v in tasks.SUPPORTED_FLAVORS.items() if k != "firefox"}
+    with mock.patch.dict(tasks.SUPPORTED_FLAVORS, flavors_without_firefox, clear=True):
+        with pytest.raises(tasks.UnsupportedFlavor):
+            tasks.extract_our_flavors([], "firefox", "100.0.1", None)
 
 
 @pytest.mark.parametrize(
--- frontend/src/views/NewRelease/index.jsx
+++ frontend/src/views/NewRelease/index.jsx
@@ -176,7 +176,6 @@ export default function NewRelease() {
       const parts = await guessPartialVersionsAction(
         selectedProduct,
         selectedBranch,
-        ver,
       );
 
       setPartialVersions(parts.data);
