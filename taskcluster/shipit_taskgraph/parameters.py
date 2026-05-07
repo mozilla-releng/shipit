d785025b4d04380307c5076d06aa9af9bf7635ca
Julien Cristau <jcristau@mozilla.com>
2026-05-07 12:34:31 +1000

    chore(ci): update parameters schema for taskgraph v23

--- taskcluster/shipit_taskgraph/parameters.py
+++ taskcluster/shipit_taskgraph/parameters.py
@@ -2,26 +2,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from typing import Optional
 
 from taskgraph.parameters import extend_parameters_schema
-from voluptuous import (
-    Any,
-    Optional,
-)
+from taskgraph.util.schema import Schema
 
 DEPLOYMENT_BRANCHES = ("dev", "production")
 
-shipit_schema = {
-    Optional("deployment_branch"): Any(str, None),
-    Optional("deploy"): Any(True, False, None),
-}
+shipit_schema = Schema.from_dict(
+    {
+        "deployment_branch": Optional[str],
+        "deploy": Optional[bool],
+    },
+    kw_only=True,
+)
 
 extend_parameters_schema(shipit_schema)
 
 
 def get_decision_parameters(graph_config, parameters):
-    """Add repo-specific decision parameters.
-    """
+    """Add repo-specific decision parameters."""
     if parameters["tasks_for"] == "github-pull-request":
         parameters["deployment_branch"] = "github-pull-request"
     elif parameters["head_ref"].startswith("refs/heads/"):
